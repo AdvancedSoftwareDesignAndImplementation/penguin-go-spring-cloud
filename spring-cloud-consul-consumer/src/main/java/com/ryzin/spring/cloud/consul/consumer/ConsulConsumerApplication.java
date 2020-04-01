6cda1632722e5f40d27e2967cfbe4c39528522bf
Ryzin <914571420@qq.com>
2020-04-01 12:12:08 +0800

    服务消费和负载（Feign）

--- spring-cloud-consul-consumer/src/main/java/com/ryzin/spring/cloud/consul/consumer/ConsulConsumerApplication.java
+++ spring-cloud-consul-consumer/src/main/java/com/ryzin/spring/cloud/consul/consumer/ConsulConsumerApplication.java
@@ -3,9 +3,11 @@ package com.ryzin.spring.cloud.consul.consumer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+@EnableFeignClients // 添加此注解开启扫描Spring Cloud Feign客户端的功能
 @SpringBootApplication
 public class ConsulConsumerApplication {
 
