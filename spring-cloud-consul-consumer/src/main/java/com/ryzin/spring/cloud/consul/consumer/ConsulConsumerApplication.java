56ba46cd690d6aa808cb2e4f4f1afaab39815f67
Ryzin <914571420@qq.com>
2020-04-01 10:13:32 +0800

    consul，支持服务注册和发现
    注册2个Consul服务提供者
    注册1个Consul服务消费者
    其中在服务消费者测试：
    1. 均衡负载
    2. 轮询

---
+++ spring-cloud-consul-consumer/src/main/java/com/ryzin/spring/cloud/consul/consumer/ConsulConsumerApplication.java
@@ -0,0 +1,12 @@
+package com.ryzin.spring.cloud.consul.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsulConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsulConsumerApplication.class, args);
+	}
+}
---
+++ spring-cloud-consul-producer2/src/main/java/com/ryzin/spring/cloud/consul/producer2/ConsulProducerApplication.java
@@ -0,0 +1,14 @@
+package com.ryzin.spring.cloud.consul.producer2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ConsulProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsulProducerApplication.class, args);
+	}
+}
