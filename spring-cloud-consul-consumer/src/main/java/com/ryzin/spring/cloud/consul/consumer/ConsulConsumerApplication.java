6f9583c4798b9339149ae4e0236087d94c0e354a
Ryzin <914571420@qq.com>
2020-04-01 13:18:48 +0800

    熔断监控面板（Hystrix Dashboard）
    测试单体Hystrix消费者
    /hystrix.stream

--- spring-cloud-consul-consumer/src/main/java/com/ryzin/spring/cloud/consul/consumer/ConsulConsumerApplication.java
+++ spring-cloud-consul-consumer/src/main/java/com/ryzin/spring/cloud/consul/consumer/ConsulConsumerApplication.java
@@ -2,10 +2,13 @@ package com.ryzin.spring.cloud.consul.consumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 
 @EnableFeignClients // 添加此注解开启扫描Spring Cloud Feign客户端的功能
 @SpringBootApplication
@@ -20,4 +23,14 @@ public class ConsulConsumerApplication {
 	public RestTemplate restTemplate() {
 		return new RestTemplate();
 	}
+	
+	@Bean
+    public ServletRegistrationBean getServlet() {
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
+    }  
 }
